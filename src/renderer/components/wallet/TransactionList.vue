04dce21a4ccb637fd9cdf5f879aa69e51e54434c
CryptoKnight <cryptoknight1988@gmail.com>
2019-02-15 13:36:24 +0000

    List transaction UI changes to include the transaction confirmation icons.

--- src/renderer/components/wallet/TransactionList.vue
+++ src/renderer/components/wallet/TransactionList.vue
@@ -1,7 +1,7 @@
 <template>
 
     <!-- Transaction List -->
-    <div v-if="wgrTransactionList.length === 0" class="no-transactions text-center">
+    <div v-if="wgrTransactionRecords.length === 0" class="no-transactions text-center">
 
         <p>Wallet has no Wagerr transactions to list...</p>
 
@@ -16,6 +16,8 @@
             <thead>
 
                 <tr>
+
+                    <th></th>
 
                     <th class="col s1 m1 l1 hide-on-med-and-down">Date</th>
 
@@ -33,21 +35,59 @@
 
             <tbody>
 
-                <tr v-for="tx in wgrTransactionList" :key="tx.id">
+                <tr v-for="tx in wgrTransactionRecords" :key="tx.id" class="tx-record">
 
-                    <td class="col s2s m2 l2 hide-on-med-and-down">
+                    <td v-if="tx.confirmations === -1" class="confirmations confirmation-conflicted">
 
-                        {{ Number(tx.time) | moment('timezone', getTimezone, 'LLLL') }}
+                        <i class="far fa-times-circle"></i>
 
                     </td>
 
-                    <td class="col s1s m1 l1 hide-on-small-only">{{tx.category}}</td>
+                    <td v-else-if="tx.confirmations === 0" class="confirmations confirmation-pending">
+
+                        <i class="far fa-question-circle"></i>
+
+                    </td>
+
+                    <td v-else-if="tx.confirmations > 0 && tx.confirmations < 6" class="confirmations">
+
+                        <div class="timer-loader"></div>
+
+
+                    </td>
+
+                    <td v-else class="confirmations confirmation-success">
+
+                        <i class="far fa-check-circle"></i>
+
+                    </td>
+
+
+                    <td class="col s2s m2 l2 hide-on-med-and-down" :class="{ 'confirmation-conflicted' : tx.confirmations === -1 }">
+
+                        {{ Number(tx.time) | moment('timezone', getTimezone, 'LLL') }}
+
+                    </td>
+
+                    <td class="col s1s m1 l1 hide-on-small-only" :class="{ 'confirmation-conflicted' : tx.confirmations === -1 }">
+
+                        {{tx.type}}
+
+                    </td>
 
                     <!--<td class="col s3s m3 l3 hide-on-med-and-down show-on-large">{{tx.blockhash}}</td>-->
 
-                    <td class="col s3s m3 l3 ">{{tx.address}}</td>
+                    <td class="col s3s m3 l3" :class="{ 'confirmation-conflicted' : tx.confirmations === -1 }">
 
-                    <td class="col s2 m2 l2 ">{{tx.amount}}</td>
+                        {{ tx.details[0].address }}
+
+                    </td>
+
+                    <td class="col s2 m2 l2" :class="{ 'confirmation-conflicted' : tx.confirmations === -1 }">
+
+                        {{ tx.amount}}
+
+                    </td>
 
                 </tr>
 
@@ -61,7 +101,7 @@
 
 <script>
 
-    import Vuex from 'vuex'
+    import Vuex from 'vuex';
 
     export default {
         name: 'TransactionList',
@@ -69,15 +109,16 @@
         computed: {
             ...Vuex.mapGetters([
                 'getTimezone',
-                'wgrTransactionList'
+                'wgrTransactionList',
+                'wgrTransactionRecords'
             ])
         },
 
         methods: {
             ...Vuex.mapActions([
                 ''
-            ]),
-        },
+            ])
+        }
     }
 
 </script>
@@ -93,6 +134,32 @@
     .no-transactions .fa-list-alt{
         color: $wagerr_dark_red;
         font-size: 10em;
+    }
+
+    .invalid{
+        font-size: 1.1em;
+        color: $wagerr_dark_red;
+    }
+
+    tbody tr{
+        font-size: 1.1em;
+    }
+
+    .confirmations{
+        padding: 15px 10px 10px 10px;
+        font-size: 2.1em;
+    }
+
+    .confirmation-conflicted{
+        color: $wagerr_red
+    }
+
+    .confirmation-success{
+        color: green;
+    }
+
+    .confirmation-pending{
+        color: orangered;
     }
 
 </style>
