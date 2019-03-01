29fb3c658fd1d065d7a3ca7aef944ce0c31fe68e
CryptoKnight <cryptoknight1988@gmail.com>
2019-03-01 10:59:11 +0000

    Added app menu options which also enabled copy paste functionality on the builds.

--- src/main/index.js
+++ src/main/index.js
@@ -5,7 +5,7 @@
 const {ipcMain} = require('electron');
 import Daemon from  './blockchain/daemon';
 import * as blockchain from './blockchain/blockchain';
-import {app, BrowserWindow, dialog} from 'electron';
+import {app, BrowserWindow, dialog, Menu} from 'electron';
 
 let path = require('path');
 
@@ -52,6 +52,93 @@ function createMainWindow () {
     mainWindow.on('closed', async () => {
         mainWindow = null;
     });
+
+    const template = [
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'pasteandmatchstyle' },
+                { role: 'delete' },
+                { role: 'selectall' }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forcereload' },
+                { role: 'toggledevtools' },
+                { type: 'separator' },
+                { role: 'resetzoom' },
+                { role: 'zoomin' },
+                { role: 'zoomout' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'close' }
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click () { require('electron').shell.openExternal('https://electronjs.org') }
+                }
+            ]
+        }
+    ];
+
+    if (process.platform === 'darwin') {
+        template.unshift({
+            label: app.getName(),
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        });
+
+        // Edit menu
+        template[1].submenu.push(
+            { type: 'separator' },
+            {
+                label: 'Speech',
+                submenu: [
+                    { role: 'startspeaking' },
+                    { role: 'stopspeaking' }
+                ]
+            }
+        );
+
+        // Window menu
+        template[3].submenu = [
+            { role: 'close' },
+            { role: 'minimize' },
+            { role: 'zoom' },
+            { type: 'separator' },
+            { role: 'front' }
+        ]
+    }
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
     // Show a popup dialog if the main window is unresponsive.
     mainWindow.on('unresponsive', () => {
