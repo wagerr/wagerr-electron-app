6f3a9b67680378fa98c28f3831eac1ea17911410
CryptoKnight <cryptoknight1988@gmail.com>
2019-03-08 13:11:32 +0000

    wagerrRPC middleware now uses the RPC credentials from the wagerr.conf if they are set, else we use default values.

--- src/renderer/services/api/wagerrRPC.js
+++ src/renderer/services/api/wagerrRPC.js
@@ -1,10 +1,19 @@
-import * as WagerrdRPC from 'wagerrd-rpc'
+import * as WagerrdRPC from 'wagerrd-rpc';
+import PropertiesReader from "properties-reader";
+import {getWagerrdPath, testnet} from "../../../main/blockchain/blockchain";
+const { ipcRenderer } = require('electron');
+
+// Get rpc credentials from the main process. These values are read from the wagerr.conf file in the main
+// process when the app is initialising. Default values are used if the wagerr.conf does not exist or doesnt
+// have the values set.
+let rpcUser = ipcRenderer.sendSync('rpc-username');
+let rpcPass = ipcRenderer.sendSync('rpc-password');
 
 export default {
   client: new WagerrdRPC({
     protocol: 'http',
-    user: 'wagerr',
-    pass: 'bethehouse',
+    user: rpcUser,
+    pass: rpcPass,
     host: '127.0.0.1',
     port: '8332'
   })
