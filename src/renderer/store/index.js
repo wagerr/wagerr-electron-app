759df002fa69b9e3bb6697a1a23bef236cce93f5
CryptoKnight <cryptoknight1988@gmail.com>
2019-03-30 20:19:15 +0000

    Removed unused includes from the main vuex store.

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 // Modules
 import wallet from './modules/wallet';
-import ui from './modules/ui';
 import transactions from './modules/transactions';
 import events from './modules/events';
 import user from './modules/user';
@@ -10,14 +9,12 @@ import betSlip from './modules/betslip';
 import cgBetSlip from './modules/cgbetslip';
 import network from './modules/network';
 
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
 
     modules: {
         wallet,
-        ui,
         events,
         user,
         betSlip,
