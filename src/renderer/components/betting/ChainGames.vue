24b621faf96db161f049edb2de1426982d4e6886
aaron@aaronkeith.com <aaron@aaronkeith.com>
2019-04-13 17:04:27 -0400

    Lotto Re-Stylized

--- src/renderer/components/betting/ChainGames.vue
+++ src/renderer/components/betting/ChainGames.vue
@@ -1,138 +1,137 @@
 <template>
 
     <div id="chain-games" class="">
-		<div class="row chain-container">
+		<div class="row chain-container content">
 		
-	    	<div class="chain-left col s4">
-		        <div class="row cg-lotto-info card z-depth-2 bg-gradient">
-		            <i class="far fa-question-circle pull-right modal-trigger chain-info" data-target="lotto-info"></i>
-		
-		            <h2 class=""><span></span>
-		
-		                Lotto Jackpot
-		
-		
-		            </h2>
-		
-		            <!-- Lotto Info Modal -->
-		            <CGLottoInfo></CGLottoInfo>
-		
-		            <div v-if="loadingCGEvent" class="text-center">
-		
-		                <spinner></spinner>
-		
-		            </div>
-		
-		            <div v-else class="cg-jackpot text-center">{{ potSize }} {{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</div>
 
-		            <div class="all-stats">
-
-		                <div class="col s4 text-center stats">
-
-		                    <h6>Potential Winnings</h6>
-
-		                    <div v-if="loadingCGEvent">
-
-		                        <spinner></spinner>
-
-		                    </div>
-
-		                    <div v-else class="cg-info">{{ getCGWinnings(potSize, 'bet') }} {{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</div>
-
-		                </div>
-
-		                <div class="col s4 text-center  stats">
-
-		                    <h6>Masternode Reward</h6>
-
-		                    <div v-if="loadingCGEvent">
-
-		                        <spinner></spinner>
-
-		                    </div>
-
-		                    <div v-else class="cg-info">{{ getCGWinnings(potSize, 'masternode') }} {{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</div>
-
-		                </div>
-
-		                <div class="col s4 text-center stats">
-
-		                    <h6>Burn</h6>
-
-		                    <div v-if="loadingCGEvent">
-
-		                        <spinner></spinner>
-
-		                    </div>
-
-		                    <div v-else class="cg-info">{{ getCGWinnings(potSize, 'burn') }} {{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</div>
-
-		                </div>
-
-		            </div>
+		        <div class="row cg-lotto-info card z-depth-2">
+		        	<div class="lotto-left col s4">
+			            <i class="far fa-question-circle pull-right modal-trigger chain-info" data-target="lotto-info"></i>
+			
+			            <h2 class=""><span></span>Lotto Jackpot</h2>
+			
+			            <div class="cg-dates text-center clearfix">
+			
+			                <h6>Game Start:</h6>{{ gameStartTime | moment("MMM Do YYYY hh:mm A") }} <h6>Game End:</h6>{{ gameEndTime | moment("MMM Do YYYY hh:mm A")}}
+			
+			            </div>
+			            
+			            <div class="bet-slip-options text-center clearfix chain-btns">
+			
+			                <button class="waves-effect waves-light btn-large wagerr-red-bg pulse" @click="placeCGLottoBet">Buy Ticket</button>
+			
+			            </div>
+			        </div>
+			        <div class="lotto-right col s8">
+			        	
 		
-		            <div class="all-stats">
-		
-		                <div class="col s4 text-center stats">
-		
-		                    <h6>Lotto ID</h6>
-		
-		                    <div v-if="loadingCGEvent">
-		
-		                        <spinner></spinner>
-		
-		                    </div>
-		
-		                    <div v-else class="cg-info">{{ gameID }}</div>
-		
-		                </div>
-		
-		                <div class="col s4 text-center  stats">
-		
-		                    <h6>Entrants</h6>
-		
-		                    <div v-if="loadingCGEvent">
-		
-		                        <spinner></spinner>
-		
-		                    </div>
-		
-		                    <div v-else class="cg-info">{{ noOfEntrants }}</div>
-		
-		                </div>
-		
-		                <div class="col s4 text-center stats">
-		
-		                    <h6>Entry</h6>
-		
-		                    <div v-if="loadingCGEvent">
-		
-		                        <spinner></spinner>
-		
-		                    </div>
-		
-		                    <div v-else class="cg-info">{{ entryFee }}</div>
-		
-		                </div>
-		
-		            </div>
-		
-		            <div class="cg-dates text-center clearfix">
-		
-		                {{ gameStartTime | moment("MMM Do YYYY hh:mm A") }} - {{ gameEndTime | moment("MMM Do YYYY hh:mm A")}}
-		
-		            </div>
+			            <!-- Lotto Info Modal -->
+			            <CGLottoInfo></CGLottoInfo>
+			
+			            <div v-if="loadingCGEvent" class="text-center">
+			
+			                <spinner></spinner>
+			
+			            </div>
+			
+			            <div v-else class="cg-jackpot text-center"><h3>Current Jackpot</h3>{{ potSize }} <span class="currency">{{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</span></div>
+	
+			            <div class="all-stats">
+	
+			                <div class="col s4 text-center stats">
+	
+			                    <h6>Potential Winnings</h6>
+	
+			                    <div v-if="loadingCGEvent">
+	
+			                        <spinner></spinner>
+	
+			                    </div>
+	
+			                    <div v-else class="cg-info">{{ getCGWinnings(potSize, 'bet') }} <span class="currency">{{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</span></div>
+	
+			                </div>
+	
+			                <div class="col s4 text-center  stats">
+	
+			                    <h6>Masternode Reward</h6>
+	
+			                    <div v-if="loadingCGEvent">
+	
+			                        <spinner></spinner>
+	
+			                    </div>
+	
+			                    <div v-else class="cg-info">{{ getCGWinnings(potSize, 'masternode') }} <span class="currency">{{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</span></div>
+	
+			                </div>
+	
+			                <div class="col s4 text-center stats">
+	
+			                    <h6>Burn</h6>
+	
+			                    <div v-if="loadingCGEvent">
+	
+			                        <spinner></spinner>
+	
+			                    </div>
+	
+			                    <div v-else class="cg-info">{{ getCGWinnings(potSize, 'burn') }} <span class="currency">{{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }}</span></div>
+	
+			                </div>
+	
+			            </div>
+			
+			            <div class="all-stats bottom-row">
+			
+			                <div class="col s4 text-center stats">
+			
+			                    <h6>Lotto ID</h6>
+			
+			                    <div v-if="loadingCGEvent">
+			
+			                        <spinner></spinner>
+			
+			                    </div>
+			
+			                    <div v-else class="cg-info">{{ gameID }}</div>
+			
+			                </div>
+			
+			                <div class="col s4 text-center  stats">
+			
+			                    <h6>Entrants</h6>
+			
+			                    <div v-if="loadingCGEvent">
+			
+			                        <spinner></spinner>
+			
+			                    </div>
+			
+			                    <div v-else class="cg-info">{{ noOfEntrants }}</div>
+			
+			                </div>
+			
+			                <div class="col s4 text-center stats">
+			
+			                    <h6>Entry</h6>
+			
+			                    <div v-if="loadingCGEvent">
+			
+			                        <spinner></spinner>
+			
+			                    </div>
+			
+			                    <div v-else class="cg-info">{{ entryFee }}</div>
+			
+			                </div>
+			
+			            </div>
 		            
-		            <div class="bet-slip-options text-center clearfix chain-btns">
-		
-		                <button class="waves-effect waves-light btn-large wagerr-red-bg pulse" @click="placeCGLottoBet">Buy Ticket</button>
-		
 		            </div>
 		
 		        </div>
-		    </div>
-		
-			<div class="chain-right col s8">
+
 		        <div class="table-container">
 		
 		            <h4>CG Lotto Bet History</h4>
@@ -140,7 +139,7 @@
 		            <c-g-lotto-bet-transaction-list></c-g-lotto-bet-transaction-list>
 		
 		        </div>
-			</div>
+
 			
 		</div>
     </div>
@@ -155,9 +154,15 @@
     import CGLottoBetTransactionList from '@/components/betting/components/CGLottoBetTransactionList';
     import CGLottoInfo from '@/components/betting/components/CGLottoInfo';
 
+    
+    
     export default {
         name: 'CGLottoBets',
         components: { CGLottoBetTransactionList, CGLottoInfo, Spinner },
+        
+        beforeCreate: function() {
+	        document.body.className = 'lotto';
+	    },
 
         computed: {
             ...Vuex.mapGetters([
