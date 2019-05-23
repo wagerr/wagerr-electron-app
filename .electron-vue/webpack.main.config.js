6fbea5f7b3b4eaa5e4440c882b4e099991f93256
Cryptarchist <cryptarchist@gmail.com>
2019-05-23 19:36:41 +0100

    Bump Babel dependencies to v7

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -6,8 +6,6 @@ const path = require('path');
 const { dependencies } = require('../package.json');
 const webpack = require('webpack');
 
-const BabiliWebpackPlugin = require('babili-webpack-plugin');
-
 let mainConfig = {
   entry: {
     main: path.join(__dirname, '../src/main/index.js')
@@ -58,7 +56,6 @@ if (process.env.NODE_ENV !== 'production') {
  */
 if (process.env.NODE_ENV === 'production') {
   mainConfig.plugins.push(
-    new BabiliWebpackPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     })
