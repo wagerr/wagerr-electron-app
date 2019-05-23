28c6c5032a486a15c5e16c62c82f22ff261943a4
wagerrdeveloper <47796377+wagerrdeveloper@users.noreply.github.com>
2019-05-23 10:09:52 -0500

    Added Team Name and Start Time to Bet History

--- src/renderer/components/bets/components/BetTransactionList.vue
+++ src/renderer/components/bets/components/BetTransactionList.vue
@@ -21,11 +21,18 @@
 
                     <th class="hide-on-small-only">Event ID</th>
 
+                    <th class="">Start Time</th>
+
                     <th class="hide-on-med-and-down show-on-large">Bet Outcome</th>
+
+                    <th class="">Home</th>
+
+                    <th class="">Away</th>
 
                     <th class=""> {{ getNetworkType === "Testnet" ? 'tWGR' : 'WGR' }} Amount</th>
 
                     <th class="">Result</th>
+
 
                 </tr>
 
@@ -37,9 +44,16 @@
 
                     <td class="hide-on-small-only">
 
-                        <a @click="blockExplorerUrl(tx['tx-id'])" class="transaction-link">
 
-                            {{ tx['tx-id'] }}
+                        <a v-clipboard="(tx['tx-id'])" @click="copiedAlert()" class="transaction-list-link tooltipped" data-position="bottom" data-tooltip="Copy">
+
+                            <i class="far fa-copy"></i>
+
+                        </a>
+
+                        <a @click="blockExplorerUrl(tx['tx-id'])" class="transaction-list-link tooltipped" data-position="bottom" data-tooltip="Open in block explorer">
+
+                            <i class="fas fa-link"></i>
 
                         </a>
 
@@ -47,11 +61,23 @@
 
                     <td class="hide-on-small-only" >{{ tx['event-id'] }}</td>
 
+                    <td class="hide-on-small-only" >{{ Number(tx.starting) | moment('timezone', getTimezone, 'LLL') }}</td>
+
                     <td class="hide-on-med-and-down show-on-large">{{ outcomeToText( tx['team-to-win'] ) }}</td>
+                    
+                    <td class="hide-on-small-only" >{{ tx['home'] }}</td>
+
+                    <td class="hide-on-small-only" >{{ tx['away'] }}</td>
 
                     <td class="">{{ tx.amount }}</td>
 
                     <td class="hide-on-small-only" >{{ tx['result'] }}</td>
+
+
+
+
+
+
 
                 </tr>
 
@@ -135,4 +161,3 @@
     }
 
 </script>
-
