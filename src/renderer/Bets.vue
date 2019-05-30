7075607a58c936ed72635eecf2eb808d4d7b9655
wagerrdeveloper <47796377+wagerrdeveloper@users.noreply.github.com>
2019-05-30 12:33:49 -0500

    Add tooltip regrading delays in new bets showing on Bet History

--- src/renderer/Bets.vue
+++ src/renderer/Bets.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="bets" class="content row">
     <div class="col s12">
-      <h4>Bet Transaction History</h4>
-
+      <h4>
+        Bet Transaction History
+        <small
+          ><i
+            class="far fa-question-circle tooltipped"
+            data-position="right"
+            data-tooltip="Your most recent bets will appear after a short time."
+          ></i
+        ></small>
+      </h4>
       <bet-transaction-list></bet-transaction-list>
     </div>
   </div>
@@ -13,6 +21,11 @@ import BetTransactionList from '@/components/bets/components/BetTransactionList'
 
 export default {
   name: 'Bets',
-  components: { BetTransactionList }
+  components: { BetTransactionList },
+
+  mounted: function() {
+    // Initialise the Material JS so modals, drop down menus etc function.
+    M.AutoInit();
+  }
 };
 </script>
