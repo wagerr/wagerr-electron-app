e731b6d5a5da12f3518e917bdeb3b78fdc512d8f
Cryptarchist <cryptarchist@gmail.com>
2019-07-23 11:21:20 +0100

    Use babel-minify on the production code

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -6,6 +6,8 @@ const path = require('path');
 const { dependencies } = require('../package.json');
 const webpack = require('webpack');
 
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+
 let mainConfig = {
   entry: {
     main: path.join(__dirname, '../src/main/index.js')
@@ -45,6 +47,7 @@ let mainConfig = {
  */
 if (process.env.NODE_ENV === 'production') {
   mainConfig.plugins.push(
+    new MinifyPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     })
