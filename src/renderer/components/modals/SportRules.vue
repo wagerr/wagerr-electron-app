10417fa4e0525aa592bdfa7054d2deda676772ca
Cryptarchist <cryptarchist@gmail.com>
2019-07-26 16:35:29 +0100

    Make sport rules modal scrollable

--- src/renderer/components/modals/SportRules.vue
+++ src/renderer/components/modals/SportRules.vue
@@ -25,6 +25,46 @@
           Aenean et tortor at risus viverra adipiscing. Vel orci porta non
           pulvinar neque laoreet.
         </p>
+
+        <h5>Soccer</h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque
+          aliquam vestibulum morbi blandit cursus risus at ultrices. Euismod
+          quis viverra nibh cras. Ornare quam viverra orci sagittis eu. Nibh
+          cras pulvinar mattis nunc. Nulla posuere sollicitudin aliquam ultrices
+          sagittis orci a. Est velit egestas dui id ornare arcu odio ut. Cras
+          adipiscing enim eu turpis egestas pretium aenean pharetra magna. Ut
+          faucibus pulvinar elementum integer enim neque. Morbi non arcu risus
+          quis varius quam quisque id diam. Aliquam sem fringilla ut morbi.
+          Placerat orci nulla pellentesque dignissim enim. Ullamcorper a lacus
+          vestibulum sed. Consequat id porta nibh venenatis cras sed felis eget.
+          Egestas congue quisque egestas diam in arcu cursus. Eget egestas purus
+          viverra accumsan in. Adipiscing elit pellentesque habitant morbi
+          tristique senectus et netus et. Vitae nunc sed velit dignissim.
+          Aenean et tortor at risus viverra adipiscing. Vel orci porta non
+          pulvinar neque laoreet.
+        </p>
+
+        <h5>Soccer</h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque
+          aliquam vestibulum morbi blandit cursus risus at ultrices. Euismod
+          quis viverra nibh cras. Ornare quam viverra orci sagittis eu. Nibh
+          cras pulvinar mattis nunc. Nulla posuere sollicitudin aliquam ultrices
+          sagittis orci a. Est velit egestas dui id ornare arcu odio ut. Cras
+          adipiscing enim eu turpis egestas pretium aenean pharetra magna. Ut
+          faucibus pulvinar elementum integer enim neque. Morbi non arcu risus
+          quis varius quam quisque id diam. Aliquam sem fringilla ut morbi.
+          Placerat orci nulla pellentesque dignissim enim. Ullamcorper a lacus
+          vestibulum sed. Consequat id porta nibh venenatis cras sed felis eget.
+          Egestas congue quisque egestas diam in arcu cursus. Eget egestas purus
+          viverra accumsan in. Adipiscing elit pellentesque habitant morbi
+          tristique senectus et netus et. Vitae nunc sed velit dignissim.
+          Aenean et tortor at risus viverra adipiscing. Vel orci porta non
+          pulvinar neque laoreet.
+        </p>
       </div>
     </div>
   </div>
@@ -38,7 +78,7 @@ export default {
 
 <style lang="scss" scoped>
 .modal {
-  overflow-y: inherit;
+  overflow-y: auto;
 }
 
 .modal-content {
