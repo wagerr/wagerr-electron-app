ac1d0a4e3b945380dbc1901756a3c19efd9ea7a3
Paul Angell <angell.paul@gmail.com>
2019-08-04 18:17:16 +0700

    Reload first page Bet Transactions for updated best made.
    - Next button will either retrieve more betting history or navigate
      pagination, depending on page.

--- src/renderer/components/bets/components/BetTransactionList.vue
+++ src/renderer/components/bets/components/BetTransactionList.vue
@@ -16,9 +16,9 @@ class="no-transactions z-depth-2 text-center"
   </div>
   
   <div v-else>
-  <div class="row">
+    <div class="row">
   <div class="input-field col s1">
-  <select class="input-field"
+  <select
 v-model="limit"
   >
   <option :value="3">3</option>
@@ -27,7 +27,7 @@ v-model="limit"
   <option :value="10000">All</option>
   </select>
   <label>List</label>
-  </div>
+  </div>      
   <paginate
 v-model="pageSelected"
 :pageCount="pageCounted"
@@ -39,37 +39,42 @@ v-model="pageSelected"
 :pageClass="'waves-effect'"
 :hide-prev-next="true">
   </paginate>
-  <a v-if="transactionMax === -1 && (pageSelected === pageCount)"
-@click="nextLot"
-  >Next</a>
+  <ul class="pagination">
+  <li>
+    <a v-if="transactionMax === -1 && (pageSelected === pageCount)"
+       tabIndex="0"
+       @click="nextLot"
+       >Next</a>
+  </li>
+  </ul>
   </div>
   <table class="main-table card z-depth-2">
   <thead>
   <tr>
   <th class="hide-on-small-only">Event ID</th>
-  
+
   <th class="hide-on-med-and-down">Transaction ID</th>
-  
+
   <th class="">Start Time</th>
-  
+
   <th class="hide-on-med-and-down show-on-large">Bet Outcome</th>
-  
+
   <th class="">Home</th>
-  
+
   <th class="">Away</th>
-  
+
   <th class="">
   {{ getNetworkType === 'Testnet' ? 'tWGR' : 'WGR' }} Amount
 </th>
-  
+
   <th class="">Result</th>
   </tr>
   </thead>
-  
+
   <tbody>
-  <tr v-for="tx in betTransactionsPaginated" :key="tx.id">
+  <tr v-for="tx in pagedList" :key="tx.id">
   <td class="hide-on-small-only">{{ tx['event-id'] }}</td>
-  
+
   <td class="hide-on-small-only">
   <a
 v-clipboard="tx['tx-id']"
@@ -136,38 +141,30 @@ export default {
       this.pageCount = Math.round(this.betTransactionsPaginated.length / this.limit);
       if ((this.pageCount * this.limit) < this.betTransactionsPaginated.length) this.pageCount += 1;
       return this.pageCount
+    },
+    pagedList: function() {
+      if (this.pageSelected === 0) {
+        return this.betTransactionsPaginated
+      } else {
+        let start = 0;
+        start = this.limit * (this.pageSelected);
+        start -= this.limit;
+        return this.betTransactionsPaginated.slice(start, start + this.limit)
+      }
     }
   },
-  
+
   methods: {
     ...Vuex.mapActions([
       'getAccountAddress',
       'getPLBetTransactionList',
       'getBetTransactionListTest'
     ]),
-    
+
     setPage: function(pageNum) {
-      let start = 0;
-      if (pageNum > 1) {
-        start = this.limit * (pageNum);
-        start -= this.limit;
-      }
-      console.log("transactions", this.betTransactionsPaginated)
-      console.log("tlist", this.tlist)
-      // this.tlist = this.betTransactionsPaginated.slice(start, start + this.limit)
+      this.pageSelected = pageNum;
     },
-    
-    // nextL: function() {
-    //   console.log("go")
-    //   this.loadPagination();
-    //   if (this.transactionMax === -1 && (this.pageSelected === this.pageCount)) {
-    //     this.pageSelected = this.pageCount + 1;
-    //   } else {
-    //     this.pageSelected = this.pageCount;
-    //   }
-    //   this.setPage(this.pageSelected);
-    // },
-    
+
     nextLot: function() {
       if (this.transactionMax === -1) {
         this.loadPagination();
@@ -179,7 +176,7 @@ export default {
         this.setPage(this.pageSelected);
       }
     },
-    
+
     // Convert the interger
     outcomeToText: function(outcome) {
       switch (outcome) {
@@ -201,55 +198,75 @@ export default {
         return outcome;
       }
     },
-    
+
     blockExplorerUrl(txId) {
       let shell = require('electron').shell;
       let explorerUrl =
           this.getNetworkType === 'Testnet'
           ? constants.TESTNET_EXP_URL
           : constants.MAINNET_EXP_URL;
-      
+
       shell.openExternal(explorerUrl + '/#/tx/' + txId);
     },
-    
-    loadPagination: function() {
+
+    loadPagination: function(pageNum = -1) {
       let from = this.betTransactionsPaginated.length === 0 ? 0 : this.betTransactionsPaginated.length;
-      let length = this.betTransactionlistLength
-      this.getPLBetTransactionList({length: length, rexg: '*', from: from, filter: ''})
+      if (pageNum > -1) {
+        from = pageNum;
+      }
+      let length = this.limit
+      this.getPLBetTransactionList({
+        length: length,
+        rexg: '*',
+        from: from,
+        filter: '',
+        betTransactionlistLength: this.limit
+      })
     }
   },
 
   data() {
     return {
       timeout: 0,
-      // transactionsPaginated: [],
-      // tlist: [],
-      transactionLimit: 0,
-      limit: 3,
+      tlist: [],
+      limit: 34,
       pageCount: 0,
-      pageSelected: 1,
-      betTransactionlistLength: 3,
-      // transactionMax: -1
+      pageSelected: 0
     };
   },
 
   watch: {
     limit: function(val) {
-      this.pageSelected = 1;
+      // fix selected page
+      this.pageSelected = 0;
       this.setPage(1)
       this.limit
+    },
+    transactionMax: function(val) {
+      if (val !== -1) {
+        if (this.plBetTransactionList.length === 0 )
+          this.pageSelected = this.pageCount;
+        else {
+          this.pageSelected = this.pageCount + 1;
+        }
+      }
     }
   },
 
   async created() {
-    await this.loadPagination();
+    await this.loadPagination(0);
     await this.setPage(1);
+  },
+
+  mounted: function() {
+    M.AutoInit();
   },
   
   mounted() {
     // Ping the get bets RPC method every 5 secs to show any new bet transactions.
     // let electronStore = new ElectronStore()
     // this.transactionsPaginated = electronStore.get('betTransactions');
+    // Initialise the Material JS so modals, drop down menus etc function.
     this.timeout = setInterval(
       async function() {
         // Todo: add filter and then retrieve
@@ -264,3 +281,9 @@ export default {
   }
 };
 </script>
+
+  <style lang="scss" scoped>
+    ul.pagination {
+    display: inline-block;
+    }
+  </style>
