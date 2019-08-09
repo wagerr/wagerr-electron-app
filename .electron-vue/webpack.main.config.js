fff2dbaa097759b3060d5eba568ffad506bb8e79
Cryptarchist <cryptarchist@gmail.com>
2019-08-09 10:14:33 +0100

    Now web builds have been removed consolidate builds to dist folder only
    Also remove the dist folder from the repo as it will be created if it
    does not exist.

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -34,7 +34,7 @@ let mainConfig = {
   output: {
     filename: '[name].js',
     libraryTarget: 'commonjs2',
-    path: path.join(__dirname, '../dist/electron')
+    path: path.join(__dirname, '../dist')
   },
   plugins: [new webpack.NoEmitOnErrorsPlugin()],
   resolve: {
