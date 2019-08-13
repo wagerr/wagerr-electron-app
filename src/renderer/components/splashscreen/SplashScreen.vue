bacbcbe4cf4423c8a5c3276058de884a6ea38fa2
Cryptarchist <cryptarchist@gmail.com>
2019-08-13 15:13:33 +0100

    Improve the performance of the Splash Screen

--- src/renderer/components/splashscreen/SplashScreen.vue
+++ src/renderer/components/splashscreen/SplashScreen.vue
@@ -61,6 +61,12 @@ let path = require('path');
 export default {
   name: 'SplashScreen',
 
+  data() {
+    return {
+      confPath: getWagerrConfPath()
+    };
+  },
+
   computed: {
     ...Vuex.mapGetters([
       'balance',
@@ -112,7 +118,7 @@ export default {
       ipcRenderer.closeWallet();
     },
 
-    getTimeBehindText: function(secs, blockchainInfo) {
+    getTimeBehindText: function(seconds, blockHeight) {
       const HOUR_IN_SECONDS = 60 * 60;
       const DAY_IN_SECONDS = 24 * 60 * 60;
       const WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
@@ -122,109 +128,127 @@ export default {
       let years;
       let remainder;
 
-      if (Math.round(secs / HOUR_IN_SECONDS) === 0) {
-        // Wallet is synced enough to all user access.
+      if (Math.round(seconds / HOUR_IN_SECONDS) === 0) {
+        // Wallet is synced enough to allow user access.
         this.updateWalletLoaded(true);
-      } else if (secs < 2 * DAY_IN_SECONDS) {
+      } else if (seconds < 2 * DAY_IN_SECONDS) {
         timeBehindText =
-          Math.round(secs / HOUR_IN_SECONDS) +
+          Math.round(seconds / HOUR_IN_SECONDS) +
           ' hours behind, Scanning block ' +
-          blockchainInfo.blocks;
-      } else if (secs < 2 * WEEK_IN_SECONDS) {
+          blockHeight;
+      } else if (seconds < 2 * WEEK_IN_SECONDS) {
         timeBehindText =
-          Math.round(secs / DAY_IN_SECONDS) +
+          Math.round(seconds / DAY_IN_SECONDS) +
           ' days behind, Scanning block ' +
-          blockchainInfo.blocks;
-      } else if (secs < YEAR_IN_SECONDS) {
+          blockHeight;
+      } else if (seconds < YEAR_IN_SECONDS) {
         timeBehindText =
-          Math.round(secs / WEEK_IN_SECONDS) +
+          Math.round(seconds / WEEK_IN_SECONDS) +
           ' weeks behind, Scanning block ' +
-          blockchainInfo.blocks;
+          blockHeight;
       } else {
-        years = secs / YEAR_IN_SECONDS;
-        remainder = secs % YEAR_IN_SECONDS;
+        years = seconds / YEAR_IN_SECONDS;
+        remainder = seconds % YEAR_IN_SECONDS;
 
         timeBehindText =
           Math.round(years) +
           ' year and ' +
           Math.round(remainder / WEEK_IN_SECONDS) +
           ' weeks behind, Scanning block ' +
-          blockchainInfo.blocks;
+          blockHeight;
       }
 
       return timeBehindText;
     },
 
-    // Check for peers every 4 seconds * 30 (2 minutes) to ensure we are
-    // connected to the network.
-    checkPeerStatus: function() {
-      return new Promise(function(resolve, reject) {
-        let count = 0;
-        let peers = 0;
+    sleep: function(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    },
 
-        let intervalId = setInterval(async function() {
-          peers = await networkRPC.getPeerInfo();
-          //console.log('peer count: ' + peers.length);
+    // Check for peers.
+    checkPeerStatus: async function() {
+      let count = 0;
+      let connections = 0;
+      let peersFound = false;
 
-          // If we have successfully connected to peers return
-          if (peers.length > 0) {
-            clearInterval(intervalId);
-            resolve(true);
-          }
+      // While no peers have connected to the daemon keep looping.
+      while (!peersFound) {
+        let peerInfo = await networkRPC.getPeerInfo();
+        connections = peerInfo.length;
 
-          // If we have no peers show a warning message to the user.
-          if (count === 30) {
-            ipcRenderer.noPeers();
-            clearInterval(intervalId);
-            resolve(true);
-          }
+        // If we have successfully connected to peers break out of the loop.
+        if (connections > 0) {
+          peersFound = true;
+          break;
+        }
 
-          count++;
-        }, 4000);
-      });
+        count++;
+
+        // Give the daemon an arbitrary 101 loops to find peers. If not show an error to the user.
+        if (count === 100) {
+          ipcRenderer.noPeers();
+          return 1;
+        }
+
+        // Sleep for 1 second between loops to lessen the burden on contiously making calls to the daemon and updating the UI.
+        await this.sleep(1000);
+      }
+
+      // Once peers have been found resolve the Promise.
+      if (peersFound) {
+        return 1;
+      }
     },
 
     // Show the blockchain sync status information.
-    syncBlockchainStatus: function() {
-      return new Promise(
-        function(resolve, reject) {
-          let secs = 1;
-          let block;
+    syncBlockchainStatus: async function() {
+      let bestBlockHash;
+      let bestBlockHeight;
+      let bestBlockTime;
+      let bestBlockTimeDifference;
+      let synced = false;
+      let verificationProgress;
 
-          // Check the status of the blockchain to see if we need to sync to the tip.
-          let intervalId = setInterval(
-            async function() {
-              let blockchainInfo = await blockchainRPC.getBlockchainInfo();
-              let bestBlockHash = blockchainInfo.bestblockhash;
+      while (!synced) {
+        let blockchainInfo = await blockchainRPC.getBlockchainInfo();
+        bestBlockHash = blockchainInfo.bestblockhash;
+        bestBlockHeight = blockchainInfo.blocks;
+        verificationProgress = blockchainInfo.verificationprogress;
 
-              block = await blockchainRPC.getBlockInfo(bestBlockHash);
-              secs = moment().diff(block.time * 1000, 'seconds');
+        let blockInfo = await blockchainRPC.getBlockInfo(bestBlockHash);
+        bestBlockTime = blockInfo.time;
+        bestBlockTimeDifference = moment().diff(
+          bestBlockTime * 1000,
+          'seconds'
+        );
 
-              let timeBehindText = this.getTimeBehindText(secs, blockchainInfo);
-              this.updateInitText(timeBehindText);
+        let timeBehindText = this.getTimeBehindText(
+          bestBlockTimeDifference,
+          bestBlockHeight
+        );
+        this.updateInitText(timeBehindText);
 
-              // if the blockchain has been fully synced.
-              if (secs < 0) {
-                // Set the network type.
-                clearInterval(intervalId);
-                resolve(true);
-              }
-            }.bind(this),
-            500
-          );
-        }.bind(this)
-      );
+        // If verification progress is 1 or above it means the daemon is synced.
+        if (verificationProgress >= 1) {
+          synced = true;
+          break;
+        }
+
+        // Sleep for 1 second between loops to lessen the burden on contiously making calls to the daemon and updating the UI.
+        await this.sleep(1000);
+      }
+
+      // Once peers have been found resolve the Promise.
+      if (synced) {
+        return 1;
+      }
     },
+
     onOpenConf: function() {
       console.log(this.confPath);
       //ipcRenderer.openConf(s);
       shell.openItem(this.confPath);
     }
-  },
-  data() {
-    return {
-      confPath: getWagerrConfPath()
-    };
   },
 
   async created() {
