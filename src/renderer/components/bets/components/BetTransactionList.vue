f07bda2d7065cd4f87f312c651376404922cbb98
Paul Angell <angell.paul@gmail.com>
2019-09-25 15:10:13 +1000

    Get it running again

--- src/renderer/components/bets/components/BetTransactionList.vue
+++ src/renderer/components/bets/components/BetTransactionList.vue
@@ -5,130 +5,133 @@ v-if="plBetTransactionList.length === 0 && betTransactionsPaginated.length === 0
 class="no-transactions z-depth-2 text-center"
   >
   <p>Looks like a new wallet, no betting transactions to list!</p>
-  
+
   <p>
-  Jump to the
-  <router-link class="router-link" tag="a" to="/betting"
-  >betting tab</router-link
-  >
-  and start placing bets.
+    Jump to the
+    <router-link class="router-link" tag="a" to="/betting"
+                 >betting tab</router-link
+                               >
+    and start placing bets.
   </p>
   </div>
-  
+
   <div v-else>
     <div class="row">
-  <div class="input-field col s1">
-  <select
-v-model="limit"
-  >
-  <option :value="3">3</option>
-  <option :value="6">6</option>
-  <option :value="100">100</option>
-  <option :value="10000">All</option>
-  </select>
-  <label>List</label>
-  </div>      
-  <paginate
-v-model="pageSelected"
-:pageCount="pageCounted"
-:pageRange="limit"
-:clickHandler="setPage"
-:prevText="'Prev'"
-:nextText="'Next'"
-:containerClass="'pagination'"
-:pageClass="'waves-effect'"
-:hide-prev-next="true">
-  </paginate>
-  <ul class="pagination">
-  <li>
-    <a v-if="transactionMax === -1 && (pageSelected === pageCount)"
-       tabIndex="0"
-       @click="nextLot"
-       >Next</a>
-  </li>
-  </ul>
+      <div class="input-field col s1">
+        <select
+          v-model="limit"
+          >
+          <option v-for="limitt in limits" v-bind:value="limitt.value">
+            {{ limitt.text }}
+          </option>
+        </select>
+        <label>List</label>
+      </div>
+      <paginate
+        v-model="pageSelected"
+        :pageCount="pageCounted"
+        :pageRange="limit"
+        :clickHandler="setPage"
+        :prevText="'Prev'"
+        :nextText="'Next'"
+        :containerClass="'pagination'"
+        :pageClass="'waves-effect'"
+        :hide-prev-next="true">
+      </paginate>
+      <ul class="pagination">
+        <li>
+          <a v-if="transactionMax === -1 && (pageSelected === pageCount)"
+             tabIndex="0"
+             @click="nextLot"
+             >Next</a>
+        </li>
+      </ul>
+    </div>
+    <table class="main-table card z-depth-2">
+      <thead>
+        <tr>
+          <th class="hide-on-small-only">Event ID</th>
+
+          <th class="hide-on-med-and-down">Transaction ID</th>
+
+          <th class="">Start Time</th>
+
+          <th class="hide-on-med-and-down show-on-large">Bet Outcome</th>
+
+          <th class="">Home</th>
+
+          <th class="">Away</th>
+
+          <th class="">
+            {{ getNetworkType === 'Testnet' ? 'tWGR' : 'WGR' }} Amount
+          </th>
+
+          <th class="">Result</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="tx in pagedList" :key="tx.id">
+          <td class="hide-on-small-only">{{ tx['event-id'] }}</td>
+
+          <td class="hide-on-small-only">
+            <a
+              v-clipboard="tx['tx-id']"
+              @click="copiedAlert()"
+              class="transaction-list-link tooltipped"
+              data-position="bottom"
+              data-tooltip="Copy"
+              >
+              <i class="far fa-copy"></i>
+            </a>
+
+            <a
+              @click="blockExplorerUrl(tx['tx-id'])"
+              class="transaction-list-link tooltipped"
+              data-position="bottom"
+              data-tooltip="Open in block explorer"
+              >
+              <i class="fas fa-link"></i>
+            </a>
+          </td>
+
+          <td class="hide-on-small-only">
+            {{ Number(tx.starting) | moment('timezone', getTimezone, 'LLL') }}
+          </td>
+
+          <td class="hide-on-med-and-down show-on-large">
+            {{ outcomeToText(tx['team-to-win']) }}
+          </td>
+
+          <td class="hide-on-small-only">{{ tx['home'] }}</td>
+
+          <td class="hide-on-small-only">{{ tx['away'] }}</td>
+
+          <td class="">{{ tx.amount }}</td>
+
+          <td class="hide-on-small-only">{{ tx['result'] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  <table class="main-table card z-depth-2">
-  <thead>
-  <tr>
-  <th class="hide-on-small-only">Event ID</th>
+</template>
 
-  <th class="hide-on-med-and-down">Transaction ID</th>
+<script>
+import Vuex from 'vuex';
+import {
+  testnetParams,
+  mainnetParams
+} from '../../../main/constants/constants';
 
-  <th class="">Start Time</th>
-
-  <th class="hide-on-med-and-down show-on-large">Bet Outcome</th>
-
-  <th class="">Home</th>
-
-  <th class="">Away</th>
-
-  <th class="">
-  {{ getNetworkType === 'Testnet' ? 'tWGR' : 'WGR' }} Amount
-</th>
-
-  <th class="">Result</th>
-  </tr>
-  </thead>
-
-  <tbody>
-  <tr v-for="tx in pagedList" :key="tx.id">
-  <td class="hide-on-small-only">{{ tx['event-id'] }}</td>
-
-  <td class="hide-on-small-only">
-  <a
-v-clipboard="tx['tx-id']"
-@click="copiedAlert()"
-class="transaction-list-link tooltipped"
-data-position="bottom"
-data-tooltip="Copy"
-  >
-  <i class="far fa-copy"></i>
-  </a>
-  
-  <a
-@click="blockExplorerUrl(tx['tx-id'])"
-class="transaction-list-link tooltipped"
-data-position="bottom"
-data-tooltip="Open in block explorer"
-  >
-  <i class="fas fa-link"></i>
-  </a>
-  </td>
-  
-  <td class="hide-on-small-only">
-  {{ Number(tx.starting) | moment('timezone', getTimezone, 'LLL') }}
-</td>
-  
-  <td class="hide-on-med-and-down show-on-large">
-  {{ outcomeToText(tx['team-to-win']) }}
-</td>
-  
-  <td class="hide-on-small-only">{{ tx['home'] }}</td>
-  
-  <td class="hide-on-small-only">{{ tx['away'] }}</td>
-  
-  <td class="">{{ tx.amount }}</td>
-  
-  <td class="hide-on-small-only">{{ tx['result'] }}</td>
-  </tr>
-  </tbody>
-  </table>
-  </div>
-  </template>
-  
-  <script>
-  import Vuex from 'vuex';
-import constants from '../../../../main/constants/constants';
 // Test:
 import ElectronStore from 'electron-store';
 import Paginate from 'vuejs-paginate';
 
 export default {
   name: 'BetTransactionList',
-  
+
   components: { Paginate },
-  
+
   computed: {
     ...Vuex.mapGetters([
       'plBetTransactionList',
@@ -229,7 +232,13 @@ export default {
     return {
       timeout: 0,
       tlist: [],
-      limit: 34,
+      limit: 3,
+      limits: [
+        {text: '3', value: '3'},
+        {text: '6', value: '6'},
+        {text: '100', value: '100'},
+        {text: '10000', value: '10000'}
+      ],
       pageCount: 0,
       pageSelected: 0
     };
@@ -260,9 +269,22 @@ export default {
 
   mounted: function() {
     M.AutoInit();
+    console.log("here mounted");
   },
-  
+
+  // created() {
+  // M.AutoInit();
+  // console.log("here created");
+  // },
+
   mounted() {
+    window.setTimeout(
+      function() {
+        M.AutoInit();
+      },
+      100
+    );
+
     // Ping the get bets RPC method every 5 secs to show any new bet transactions.
     // let electronStore = new ElectronStore()
     // this.transactionsPaginated = electronStore.get('betTransactions');
