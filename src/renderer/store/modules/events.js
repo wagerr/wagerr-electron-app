a991724f4f711843551dcc662c27a486b0cdbd61
f-CJ <fcj.developer@gmail.com>
2019-10-13 20:23:27 +0200

    Filters events that have no markets (all odd markets are 0/0/0)

--- src/renderer/store/modules/events.js
+++ src/renderer/store/modules/events.js
@@ -22,22 +22,43 @@ const getters = {
   }
 };
 
-function filterEventsByStartingTime(events) {
+function filterEventsForCache(events) {
   // Filter events that are expired (15 mins before event start time)
-  return events.filter(e => {
-    // Prevent events that:
-    // - start in less than 12 mins 
-    // - and events starting outside the two weeks listed.
-    return  e.starting - 12 * 60 > moment().unix() &&
-            e.starting < moment().add(13, 'days').unix();
-
-  }).sort((x, y) => x.starting - y.starting);  
+  return events
+    .filter(e => {
+      // Prevent events that:
+      // - start in less than 12 mins
+      // - and events starting outside the two weeks listed.
+      return (
+        e.starting - 12 * 60 > moment().unix() &&
+        e.starting <
+          moment()
+            .add(13, 'days')
+            .unix() &&
+        // We also filter all events that have 0/0/0 for all three markets odds
+        !(
+          e.odds[0].mlAway === 0 &&
+          e.odds[0].mlDraw === 0 &&
+          e.odds[0].mlHome === 0 &&
+          e.odds[1].spreadAway === 0 &&
+          e.odds[1].spreadHome === 0 &&
+          e.odds[1].spreadPoints === 0 &&
+          e.odds[2].totalsOver === 0 &&
+          e.odds[2].totalsPoints === 0 &&
+          e.odds[2].totalsUnder === 0
+        )
+      );
+    })
+    .sort((x, y) => x.starting - y.starting);
 }
 
-function filterEventsByTournament(events, state) {
+function filterEventsByTournament(events, tournamentFilter) {
+  if (!tournamentFilter) return events;
+
   return events.filter(e => {
-    return  !state.eventsTournamentFilter || e.tournament === state.eventsTournamentFilter;
-  });  
+    // In case there is a tournament selected, we filter by the tournament
+    return e.tournament === tournamentFilter;
+  });
 }
 
 function treatListEventErr(err) {
@@ -52,8 +73,8 @@ const actions = {
   updateEventsTournamentFilter({ commit, state }, tournament) {
     commit('setEventsTournamentFilter', tournament);
   },
-  
-  //TODO it would be nice to be able to send empty/null parameters to the rpc api, now we have to call twice to the library like below (code duplication):
+
+  // TODO it would be nice to be able to send empty/null parameters to the rpc api, now we have to call twice to the library like below (code duplication):
   // * One option is to modify wagerrd-rpc library so it ignores null/undefined parameters, the only caveat is that all parameters after a null have to be null as well
   // * Another option is to modify the endpoint so it can interpret some string as an empty value
   listEvents({ dispatch, commit, state }, filter) {
@@ -62,29 +83,39 @@ const actions = {
         wagerrRPC.client
           .listEvents(state.eventsSportFilter)
           .then(function(resp) {
-            let events = filterEventsByStartingTime(resp.result);
+            // We filter events and update the cache
+            let events = filterEventsForCache(resp.result);
             dispatch('updateEventsDataCache', events, { root: true });
-            events = filterEventsByTournament(events, state);
+            // We filter events that will be shown to the user
+            events = filterEventsByTournament(
+              events,
+              state.eventsTournamentFilter
+            );
             commit('setEventsList', events);
             resolve();
           })
           .catch(function(err) {
             treatListEventErr(err, reject);
-            reject(err);            
+            reject(err);
           });
       } else {
         wagerrRPC.client
           .listEvents()
           .then(function(resp) {
-            let events = filterEventsByStartingTime(resp.result);
+            // We filter events jand update the cache
+            let events = filterEventsForCache(resp.result);
             dispatch('updateEventsDataCache', events, { root: true });
-            events = filterEventsByTournament(events, state);
+            // We filter events that will be shown to the user
+            events = filterEventsByTournament(
+              events,
+              state.eventsTournamentFilter
+            );
             commit('setEventsList', events);
             resolve();
           })
           .catch(function(err) {
             treatListEventErr(err, reject);
-            reject(err);            
+            reject(err);
           });
       }
     });
