f5e825950ad46f85c24c5dd29679d5dfb6aae8a0
Cryptarchist <cryptarchist@gmail.com>
2020-06-08 21:49:54 +0100

    Add in process for notarizing Mac builds

---
+++ build/mac/notarize.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'com.wagerr.electron.app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASS,
+    ascProvider: process.env.APPLE_PROVIDER_SHORT_NAME
+  });
+};
