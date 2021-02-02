56f8e14754d1725a1ab422c2eaf5c0ceccc1d286
Cryptarchist <cryptarchist@gmail.com>
2021-02-02 16:57:42 +0000

    Cleanup spread and total display in bet popover tooltip

--- src/renderer/components/BetHistory/BetText.vue
+++ src/renderer/components/BetHistory/BetText.vue
@@ -27,25 +27,40 @@ export default {
         case 3:
           return `Draw @ ${this.convertOdds(leg.lockedEvent.drawOdds)}`;
         case 4:
-          let oddsHome =
-            leg.lockedEvent.spreadHomeOdds > leg.lockedEvent.spreadAwayOdds ? '+' : '-';
-          oddsHome += leg.lockedEvent.spreadPoints / 10;
-          return `Home Spread ${oddsHome}@${this.convertOdds(leg.lockedEvent.spreadHomeOdds)}`;
+          return `Home Spread ${this.spreadCalc(true, leg.lockedEvent)}@${this.convertOdds(leg.lockedEvent.spreadHomeOdds)}`;
         case 5:
-          let oddsAway =
-            leg.lockedEvent.spreadAwayOdds > leg.lockedEvent.spreadHomeOdds ? '+' : '-';
-          oddsAway += leg.lockedEvent.spreadPoints / 10;
-          return `Away Spread ${oddsAway}@${this.convertOdds(leg.lockedEvent.spreadAwayOdds)}`;
+          return `Away Spread ${this.spreadCalc(false, leg.lockedEvent)}@${this.convertOdds(leg.lockedEvent.spreadAwayOdds)}`;
         case 6:
-          return `Total Over ${leg.lockedEvent.totalPoints / 10}@${this.convertOdds(
+          return `Total Over ${leg.lockedEvent.totalPoints / 100}@${this.convertOdds(
             leg.lockedEvent.totalOverOdds
           )}`;
         case 7:
-          return `Total Under ${leg.lockedEvent.totalPoints / 10}@${this.convertOdds(
+          return `Total Under ${leg.lockedEvent.totalPoints / 100}@${this.convertOdds(
             leg.lockedEvent.totalUnderOdds
           )}`;
         default:
           return leg.outcome;
+      }
+    },
+
+    spreadCalc(homeTeam, event) {
+      let homeTeamSpread;
+      let awayTeamSpread;
+
+      if (event.spreadPoints > 0) {
+        homeTeamSpread = event.spreadPoints / 100;
+        awayTeamSpread = (event.spreadPoints * -1) / 100;
+      } else {
+        homeTeamSpread = event.spreadPoints / 100;
+        awayTeamSpread = (event.spreadPoints * -1) / 100;
+      }
+
+      if (homeTeam) {
+        return homeTeamSpread;
+      }
+
+      if (!homeTeam) {
+        return awayTeamSpread;
       }
     }
   }
